33df335c904339aa8a0db1231409c3b03cda8613
Alexander Hamilton <67675434+Alexhamilton1221@users.noreply.github.com>
2023-10-04 22:10:10 -0600

    Update and rename main.java to Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@
 # CMPT 360
 *------------------------------------------------------*/ 
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,15 +16,13 @@ public class Main {
         /** Instance of test_functions class */
         Test_Functions testfunctionInstance = new Test_Functions();
 
-        /** Ask the user for a test run or regular run */
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter 't' or 'T' to test properties or any other key to get properties: ");
-        String input = scanner.nextLine();
-
-        if (input.equalsIgnoreCase("t")) {
-            testfunctionInstance.test_get_properties();
-        } else {
-            functionInstance.getProperties();
-        }
+      
+       
+        testfunctionInstance.test_get_properties();
+        functionInstance.getProperties();
+        
     }
 }
+/* TODO Rule of thumb: The supporting classes should not contain any print statements or those that ask for user input */
+/* Need to move print statements and such to main */
+/* */
